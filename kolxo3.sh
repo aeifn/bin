62aa3e9342eebdeb54ccbbe9e0d6dd1db31bf83c
Egor Kuzmichev <egor@math.ru>
2021-06-10 16:56:59 +0300

    kolxo3

--- kolxo3.sh
+++ kolxo3.sh
@@ -3,40 +3,41 @@
 BASE_DIR=$HOME/kolxo3
 UNION_DIR=$BASE_DIR/all
 DOWNLOAD_DIR=/home/transmission/Downloads/KoLXo3
+DOAS=doas
 
 kol_umount(){
-while sudo umount $UNION_DIR; do true; done;
+while $DOAS umount $UNION_DIR; do true; done;
 
 for a in /dev/md[0-9]*
 	do
 		NUM=${a##*md}
-		sudo umount $a
-		sudo mdconfig -d -u $NUM
+		$DOAS umount $a
+		$DOAS mdconfig -d -u $NUM
 	done
-	sudo rmdir $BASE_DIR/*
+	$DOAS rmdir $BASE_DIR/*
 }
 
 kol_mount() {
-sudo mkdir -p $BASE_DIR
+$DOAS mkdir -p $BASE_DIR
 
 cd $DOWNLOAD_DIR
 for ISO in disc*.iso;
 do
 	DISC_NAME=${ISO%.*}
 	DISC_DIR=$BASE_DIR/$DISC_NAME
-	MD_DEV=`sudo mdconfig -f $ISO` 
-	sudo mkdir -p $DISC_DIR
-	sudo mount_cd9660 /dev/$MD_DEV $DISC_DIR
+	MD_DEV=`$DOAS mdconfig -f $ISO` 
+	$DOAS mkdir -p $DISC_DIR
+	$DOAS mount_cd9660 /dev/$MD_DEV $DISC_DIR
 done
 }
 
+# pkg install fusefs-uinonfs
+# sysctl vfs.usermount=1
 kol_union() {
 	cd $BASE_DIR
-	sudo mkdir -p $UNION_DIR
-	for DISC_DIR in disc[012]*
-	do
-	sudo mount_unionfs -o rw -o noatime $DISC_DIR $UNION_DIR
-	done
+	mkdir -p $UNION_DIR
+	MOUNT_OPT=$(ls -d disc* | tr '\n' ':' | sed 's/:$//')
+	$DOAS unionfs -o allow_other $MOUNT_OPT $UNION_DIR
 }
 
 case $1 in
